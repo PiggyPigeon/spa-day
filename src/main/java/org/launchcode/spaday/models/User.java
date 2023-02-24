03e2c74f1a5616ce234a8fa30e6e987fb6c012dc
“Bethany <Bethanygellington@gmail.com>
2023-02-23 22:31:51 -0500

    wrote the studio but created a new password verification bug

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min=5, max=15, message="Username must be longer than 5 characters but no longer than 15.")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 6, message="password must contain at least 6 characters")
     private String password;
 
     public User() {
